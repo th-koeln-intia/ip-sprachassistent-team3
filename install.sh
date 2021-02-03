1a0ff2a8e966badc039d23b723afeac8674f9727
Philipp <xfgd1337@gmail.com>
2021-02-03 20:17:24 +0000

    update install

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt-get install -y git mpd mpc
+sudo apt-get install -y git mpd mpc curl
+
+curl -sSL https://get.docker.com | sh
+sudo usermod -a -G docker $USER
+sudo apt-get install docker-compose
 
 git clone https://github.com/respeaker/seeed-voicecard.git
 cd seeed-voicecard
@@ -12,7 +16,7 @@ cd ..
 
 git clone https://github.com/th-koeln-intia/ip-sprachassistent-team3.git apollo
 
-sudo cp apollo/install/data/radiostations.m3u /var/lib/mpd/playlists/radiostations.m3u
+sudo cp apollo/docker-service/data/radiostations.m3u /var/lib/mpd/playlists/radiostations.m3u
 sudo chmod 777 /var/lib/mpd/playlists/radiostations.m3u
 
 echo "Please reboot your pi"
