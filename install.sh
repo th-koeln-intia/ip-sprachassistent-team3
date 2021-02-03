4b01d72f078f1cc3f17cf5316a74852ca51bfd6e
Philipp <xfgd1337@gmail.com>
2021-02-03 20:20:58 +0000

    update

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo ./install.sh
 
 cd ..
 
-git clone https://github.com/th-koeln-intia/ip-sprachassistent-team3.git /apollo
+git clone https://github.com/th-koeln-intia/ip-sprachassistent-team3.git ~/apollo-voiceassistant
 
 sudo cp apollo/docker-service/data/radiostations.m3u /var/lib/mpd/playlists/radiostations.m3u
 sudo chmod 777 /var/lib/mpd/playlists/radiostations.m3u
