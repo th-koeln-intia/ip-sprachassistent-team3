b8ff9ff4597123ae0b2eadeb5edbecc6ec65d647
Philipp <xfgd1337@gmail.com>
2021-02-04 10:52:11 +0000

    update

---
+++ config/auto_install/data/nodered/data/node_modules/node-red-contrib-mpd/CHANGELOG.md
@@ -0,0 +1,22 @@
+### 1.3.0
+
+**Changes**
+- complete message is passed through.
+- allow to output raw message in case the parser of the underlying library does fail.
+
+
+### 1.2.3
+
+**Fixes**
+- ensure event emitter accepts infinitely many nodes.
+
+**Changes**
+- new homemade icon.
+
+### 1.2.0
+
+**Changes**
+
+- MPD nodes now have their own palette category.
+- If the mpd-out node receives a message with a topic and the topic field is empty in the configuration of the node, the received topic is transmitted to the output.
+- Better info texts.
---
+++ config/auto_install/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt-get install -y git mpd mpc curl
+
+curl -sSL https://get.docker.com | sh
+sudo usermod -a -G docker $USER
+sudo apt-get install docker-compose
+
+git clone https://github.com/respeaker/seeed-voicecard.git
+cd seeed-voicecard
+sudo ./install.sh
+
+cd ..
+
+git clone https://github.com/th-koeln-intia/ip-sprachassistent-team3.git ~/apollo-voiceassistant
+
+sudo cp ~/apollo-voiceassistant/config/auto_install/data/radiostations.m3u /var/lib/mpd/playlists/radiostations.m3u
+sudo chmod 777 /var/lib/mpd/playlists/radiostations.m3u
+
+sudo cp ~/apollo-voiceassistant/config/auto_install/. ~/apollo
+sudo rm -R ~/apollo-voiceassistant
+
+echo "Please reboot your pi"
